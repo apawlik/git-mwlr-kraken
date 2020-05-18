1574e784f7483ea6f85110f34f179f122f282b5c
Aleksandra Pawlik <aleksandra.n.pawlik@gmail.com>
2020-05-18 12:04:53 +1200

    A bit more analysis and .gitignore to ignore some files

--- data-analysis.R
+++ data-analysis.R
@@ -1,7 +1,6 @@
 # Short R script adapted from Software Carpentry data lesson 
 library("ggplot2")
 
-download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/master/data/gapminder_data.csv", destfile = "gapminder_data.csv")
 gapminder <- read.csv("gapminder_data.csv", header=TRUE)
 
 summary(gapminder$country)
@@ -13,6 +12,10 @@ seAsia <- c("Myanmar","Thailand","Cambodia","Vietnam","Laos")
 ## convert from a factor to a character;
 ## and get just the non-repeated elements
 countries <- unique(as.character(gapminder$country))
+
+#Find countries where life expectancy is below the average.
+below_average <- gapminder$lifeExp < 70.5
+
 
 
 ggplot(data = gapminder, mapping = aes(x = gdpPercap, y = lifeExp)) +
